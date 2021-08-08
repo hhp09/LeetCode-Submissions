5708285d2d1b318de7064d104473d19f820d3dc3
Hritik <hritik81298@gmail.com>
2021-08-07 20:10:29 -0500

    Time: 5 ms (90.03%), Space: 48.4 MB (27.57%) - LeetHub

--- find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
+        /*
         Map<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
         for(int i: nums) {
@@ -10,6 +11,27 @@ class Solution {
             if(map.get(i) == 2) {
                 list.add(i);
             }
+        }
+        
+        return list;
+        */
+        List<Integer> list = new ArrayList<>();
+        // integers are in the range 1 to n
+        // so basically, we reference the index of current element's value - 1 and make it negative
+        // if a repeated number is encountered, we will see that its index value is -
+        /*
+        nums[i] = 4;
+        index = 4 - 1 = 3;
+        nums[3] = 7;
+        if(7 < 0) add to list
+        nums[index] = -nums[index];
+        */
+        for(int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] < 0) {
+                list.add(index + 1);
+            }
+            nums[index] = -nums[index];
         }
         
         return list;
