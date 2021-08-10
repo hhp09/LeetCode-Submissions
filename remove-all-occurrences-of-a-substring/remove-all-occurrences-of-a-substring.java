afc4125ea662126e69ba0be2b8ad55be7dd4e007
Hritik <hritik81298@gmail.com>
2021-08-10 15:37:26 -0500

    Time: 5 ms (45.98%), Space: 39.6 MB (34.25%) - LeetHub

---
+++ remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        // use StringBuffer
+         boolean found = true;
+
+        StringBuffer sb=new StringBuffer(s);
+        int len=part.length();
+
+        while(found){
+            
+            found=false;
+            
+            for(int i=0;sb.length()-i>=len;i++){
+                
+                String sub=sb.substring(i,i+len);
+                
+                if(sub.equals(part)){
+                    
+                    sb.delete(i,i+len);
+                    found=true;
+                    break;
+                    
+                }
+            }
+            
+            if(!found)
+                break;
+        }
+
+        return String.valueOf(sb);
+        
+    }
+}
