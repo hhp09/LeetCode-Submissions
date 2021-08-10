43663026d7264d79f56213c6ba97251ece2c0790
Hritik <hritik81298@gmail.com>
2021-08-10 15:02:07 -0500

    Time: 6 ms (80.38%), Space: 39.1 MB (66.16%) - LeetHub

---
+++ isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s == null || t == null){
+            return false;
+        }
+        if(s.length() == 0 || t.length() == 0){
+            return true;
+        }
+        
+        Map<Character,Character> corr = new HashMap<Character,Character>();
+        
+        for(int i = 0; i < s.length(); i ++){
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            if(corr.containsKey(sc)){
+                if(corr.get(sc) != tc){
+                    return false;
+                }
+            }
+            else{
+                if(corr.containsValue(tc)){
+                    return false;   
+                }
+                else{
+                    corr.put(sc,tc);
+                }
+            }
+        }
+        return true;
+    }
+}
